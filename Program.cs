060c314e35bee604fc00cf3d2ee7896e402d509c
Starinin Andrey <starininandrey@gmail.com>
2022-11-27 18:14:39 +0300

    Update Program.cs

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using SingleResponsibilityPrinciple;
+
+var person = new Person
+{
+    FirstName = "Andrey",
+    LastName = "Starinin",
+    DateOfBirth = new DateTime(1986, 2, 18)
+};
+
+ConsolePrint.PrintInfo($"Персона: {person}");
