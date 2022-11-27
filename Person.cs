808ab744bb6f6431a1bca083d43b443c4c403306
Starinin Andrey <starininandrey@gmail.com>
2022-11-27 18:13:19 +0300

    Add Person.cs

---
+++ Person.cs
@@ -0,0 +1,13 @@
+﻿namespace SingleResponsibilityPrinciple;
+
+public class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime DateOfBirth { get; set; }
+
+    public override string ToString()
+    {
+        return $"{LastName} {FirstName}, {DateOfBirth:d}";
+    }
+}
