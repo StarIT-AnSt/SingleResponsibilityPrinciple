3a69d26f8101db7c9e5969c928b52fb6455934ab
Starinin Andrey <starininandrey@gmail.com>
2022-11-27 18:14:25 +0300

    Add ConsolePrint.cs

---
+++ ConsolePrint.cs
@@ -0,0 +1,21 @@
+﻿namespace SingleResponsibilityPrinciple;
+
+public static class ConsolePrint
+{
+    private static void Print(string message, ConsoleColor color)
+    {
+        Console.ForegroundColor = color;
+        Console.WriteLine(message);
+        Console.ResetColor();
+    }
+
+    public static void PrintInfo(string message)
+    {
+        Print(message, ConsoleColor.Blue);
+    }
+    
+    public static void PrintError(string message)
+    {
+        Print(message, ConsoleColor.Red);
+    }
+}
